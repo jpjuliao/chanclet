7c819c07a837d01c7b79fcf58eeae913c1c39b03
jpjuliao <jpjuliao@gmail.com>
2019-05-13 16:27:58 -0500

    webpack bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const MergeIntoSingleFilePlugin = require('webpack-merge-and-include-globally');
 
 module.exports = {
-  entry: {
-      fullpage: './node_modules/fullpage.js/dist/fullpage.min.js',
-      fullpageScrolloverflow: './node_modules/fullpage.js/vendors/scrolloverflow.min.js',
-},
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  }
+    entry: {
+        fullpage: './node_modules/fullpage.js/dist/fullpage.min.js',
+        scrolloverflow: './node_modules/fullpage.js/vendors/scrolloverflow.min.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]'
+    },
+    plugins: [
+        new MergeIntoSingleFilePlugin({
+            files: {
+                "bundle.js": [
+                    path.resolve(__dirname, 'node_modules/fullpage.js/dist/fullpage.min.js'),
+                    path.resolve(__dirname, 'node_modules/fullpage.js/vendors/scrolloverflow.min.js')
+                ],
+                "bundle.css": [
+                    path.resolve(__dirname, 'node_modules/fullpage.js/dist/fullpage.min.css'),
+                ]
+            }
+        })
+    ]
 };
